83bb127d7d0ac6de8219d69a2a5139262baa2fb8
Islam Assanov <82131413+islamborghini@users.noreply.github.com>
2021-11-12 21:15:51 +0600

    Update BinarySearch.cpp

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -19,14 +19,15 @@ int n,k,a[N], ans;
 int binSearch(int l, int r){
 	while(l<=r){
 	int mid = l+(r-l)/2;
-		if (a[mid] == k) {
-			return mid+1; 
-			break;}
-		else if (a[mid] < k)
-			 l = mid+1;
-		else r = mid-1;
-			   }
-		return -1;	
+	if (a[mid] == k) {
+	return mid+1; 
+	break;
+	}
+	else if (a[mid] < k)
+	l = mid+1;
+	else r = mid-1;
+	}
+	return -1;	
 			   
 }
 
